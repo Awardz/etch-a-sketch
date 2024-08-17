2e13aaa1e2a61b35279750d87a7f9f8394620621
Awardz <91509219+Awardz@users.noreply.github.com>
2024-08-16 20:04:23 -0400

    Unable to get the last part of step 4 to work.  Number of boxes are never symmetrical.

--- javascript.js
+++ javascript.js
@@ -1,34 +1,65 @@
 const container = document.getElementById("container");
 //const content = document.createElement("div");
 
-for(let i = 0; i < 16; i++)
-{ 
-    for(let j = 0; j < 16; j++)
-    {
-        const content = document.createElement("div");
-        content.style.border = "6px solid black";
-        content.classList.add("grid");
-        content.style.width = "30px";
-        content.style.height = "30px";
-        container.appendChild(content);        
-    }
+function newBoard(num = 16)
+{
+    let dimension = 960 / num + "px";
     
+    for(let i = 0; i < num; i++)
+    { 
+        for(let j = 0; j < num; j++)
+        {
+            const content = document.createElement("div");
+            
+            content.style.border = "6px solid black";
+            content.classList.add("grid");
+            content.textContent = j + 1;
+            content.style.width = dimension; 
+            content.style.height = dimension;
+            container.appendChild(content);        
+        }
+        
+    }
 }
 
-const divs = document.querySelectorAll(".grid");
-divs.forEach((div) => 
-{   
-    div.addEventListener("mouseover", () =>
-    {
-        div.style.background = "purple";
-    })
-});
 
-divs.forEach((div) => 
+// Step 3
+function hover()
+{
+    const divs = document.querySelectorAll(".grid");
+    divs.forEach((div) => 
+    {   
+        div.addEventListener("mouseover", () =>
+        {
+            div.style.background = "purple";
+        })
+    });
+
+    divs.forEach((div) => 
+    {   
+        div.addEventListener("mouseout", () =>
+        {
+            div.style.background = "white";
+        })
+    });
+}
+
+newBoard();
+hover();
+
+
+// Step 4
+const button = document.querySelector("#btn");
+button.addEventListener("click", alertFunction);
+
+function alertFunction()
 {   
-    div.addEventListener("mouseout", () =>
-    {
-        div.style.background = "white";
-    })
-});
+    let num = 0;
+    while(num < 1 || num > 100)
+        num = prompt("Enter the number of squares:");
+    container.innerHTML = "";
+
+    newBoard(num);
+    hover();
+}
 
